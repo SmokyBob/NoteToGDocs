5bd3763a59e7a6f96d6c45303287a36216210413
Mauro Solcia <mauro.solcia@gmail.com>
2013-01-02 18:23:54 +0100

    Clean Up Fixes for better user Experience

--- src/com/smokybob/NoteToGDocs/MainActivity.java
+++ src/com/smokybob/NoteToGDocs/MainActivity.java
@@ -1,6 +1,10 @@
 package com.smokybob.NoteToGDocs;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
@@ -8,12 +12,10 @@ import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecovera
 import com.google.api.client.http.ByteArrayContent;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.Drive.Files.Insert;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.smokybob.NoteToGDocs.R;
-import com.smokybob.NoteToGDocs.R.id;
-import com.smokybob.NoteToGDocs.R.layout;
-import com.smokybob.NoteToGDocs.R.menu;
 
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,7 +24,6 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.NotificationCompat;
@@ -35,29 +36,14 @@ public class MainActivity extends Activity {
 	static final int REQUEST_ACCOUNT_PICKER = 1;
 	static final int REQUEST_AUTHORIZATION = 2;
 
-	private static Uri fileUri;
 	private static Drive service;
 	private GoogleAccountCredential credential;
 	private SharedPreferences settings;
 	public static final String PREFS_NAME = "MyPrefsFile";
 	/** text/plain MIME type. */
-	private static final String TEXT_PLAIN = "text/plain";
+	private static final String SOURCE_MIME = "text/plain";
 
-	private void checkCredential() {
-		//Try to get Account Name from previous selection
-		String accountName = settings.getString("accountName", "");
-		credential = GoogleAccountCredential.usingOAuth2(this, DriveScopes.DRIVE);
-		if (accountName.length()>0) {
-			credential.setSelectedAccountName(accountName);
-			service = getDriveService(credential);
-			setContentView(R.layout.activity_main);
-		}
-		else
-		{
-			//Ask the user for credentials
-			startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
-		}
-	}
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -93,8 +79,9 @@ public class MainActivity extends Activity {
 			//FIXME: Enable Account Selection and other configurations
 			// https://code.google.com/p/google-drive-sdk-samples/source/browse/android/src/com/example/android/notepad/Preferences.java
 			//Open Settings Activity
-			//        	Intent settings = new Intent(this,SettingsActivity.class);
-			//        	startActivity(settings);
+			Intent settings = new Intent(this,SettingsActivity.class);
+			startActivity(settings);
+			toRet=true;
 			//        default:
 			//            return super.onOptionsItemSelected(item);
 			break;
@@ -133,6 +120,19 @@ public class MainActivity extends Activity {
 			//	      }
 		}
 	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		//FIXME quando vado nei settings questa proprietà proc viene chiamata, sistemare
+		EditText editText1 = (EditText) findViewById(R.id.editText1);
+		String content = editText1.getText().toString();
+		//Check text Input and if present upload the document
+		if (content.length()>0)
+			UploadNote();
+	}
+	
+	
 
 	private void ClearNote(){
 		//MSO - 20121127 - Clean the text
@@ -141,35 +141,38 @@ public class MainActivity extends Activity {
 	}
 
 	private void UploadNote(){
-		//TODO - MSO - 20121127 - Lanciare il salvataggio async
+		//Async Thread to Save the Note
 		Thread t = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				//FIXME - Async Save, not Sync
 				File newFile = new File();
 
 				//Generate Title
-				//TODO - From config Get the structure
-				String fileTitle ="Test_noteGDocs";
+				String fileTitle = getFileName();
+				//Set File Info and text
 				newFile.setTitle(fileTitle);
-				newFile.setMimeType(TEXT_PLAIN);
+				newFile.setMimeType(SOURCE_MIME);
 				EditText editText1 = (EditText) findViewById(R.id.editText1);
 				String content = editText1.getText().toString();
 
 				try {
 					File insertedFile = null;
-
+					//Upload the file
 					if (content != null && content.length() > 0) {
-						service = getDriveService(credential);
-						insertedFile = service.files()
-								.insert(newFile, ByteArrayContent.fromString(TEXT_PLAIN, content))
-								.execute();
+						Insert fileRequest = service.files()
+								.insert(newFile, ByteArrayContent.fromString(SOURCE_MIME, content));
+						//Auto Convert to Google Docs
+						fileRequest.setConvert(true);
+						insertedFile = fileRequest.execute();
 					} else {
 						insertedFile = service.files().insert(newFile).execute();
 					}
-					
+
 					//TODO: Use string resources for different languages support
-					createNotification(fileTitle, "File Uploaded correctly", insertedFile.getId());
+					createNotification(fileTitle, "File Uploaded", insertedFile.getId());
+					//Cleat and Close the application
+					editText1.setText("");//ClearNote Can't be used as it generate an exception because this code is in another thread
+					finish();
 				} catch (UserRecoverableAuthIOException e) {
 					startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
 				} catch (IOException e) {
@@ -187,39 +190,71 @@ public class MainActivity extends Activity {
 		.build();
 	}
 
-	public void createNotification(String title, String messageText, String fileId) {
+	private void createNotification(String title, String messageText, String fileId) {
 		NotificationCompat.Builder mBuilder =
 				new NotificationCompat.Builder(this)
 		.setSmallIcon(R.drawable.ic_launcher)
 		.setContentTitle(title)
 		.setContentText(messageText);
-		
-		if (fileId.length()>0){
-			Intent resultIntent = new Intent( "com.google.android.apps.drive.DRIVE_OPEN");
 
-			resultIntent.setType("text/plain");
-			resultIntent.putExtra("EXTRA_FILE_ID", fileId);
+		if (fileId.length()>0){
+
+			Intent resultIntent = new Intent(Intent.ACTION_VIEW); 
+			Uri uri = Uri.parse("https://docs.google.com/document/d/"+fileId); 
+			resultIntent.setData(uri);
 
 			// Because clicking the notification opens a new ("special") activity, there's
 			// no need to create an artificial back stack.
 			PendingIntent resultPendingIntent =
-			    PendingIntent.getActivity(
-			    this,
-			    0,
-			    resultIntent,
-			    PendingIntent.FLAG_UPDATE_CURRENT
-			);
-			
+					PendingIntent.getActivity(
+							this,
+							0,
+							resultIntent,
+							PendingIntent.FLAG_UPDATE_CURRENT
+							);
+
+			mBuilder.setDefaults(Notification.DEFAULT_ALL);		
+			mBuilder.setAutoCancel(true);
 			mBuilder.setContentIntent(resultPendingIntent);
 		}
-		
+
 		// Sets an ID for the notification
 		int mNotificationId = 001;
 		// Gets an instance of the NotificationManager service
 		NotificationManager mNotifyMgr = 
-		        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+				(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		// Builds the notification and issues it.
 		mNotifyMgr.notify(mNotificationId, mBuilder.build());
 	}
 
+	private String getFileName(){
+		//TODO - From config Get the structure
+		String toRet="";
+		//Default YYYYMMDD_HHmmss_Note
+		DateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+		// Get the date today using Calendar object.
+		Date today = Calendar.getInstance().getTime();        
+		// Using DateFormat format method we can create a string 
+		// representation of a date with the defined format.
+		toRet = df.format(today)+"_Note";
+		
+		return toRet;
+	}
+	
+	private void checkCredential() {
+		//Try to get Account Name from previous selection
+		String accountName = settings.getString("accountName", "");
+		credential = GoogleAccountCredential.usingOAuth2(this, DriveScopes.DRIVE);
+		if (accountName.length()>0) {
+			credential.setSelectedAccountName(accountName);
+			service = getDriveService(credential);
+			setContentView(R.layout.activity_main);
+		}
+		else
+		{
+			//Ask the user for credentials
+			startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+		}
+	}
 }
